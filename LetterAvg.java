73b708192dc966a93c2934b80cc9c0bc5bc4f453
Alexander Caesar <alexjcaesar@yahoo.com>
2019-10-08 15:53:09 -0500

    added getters and finshed getLetterAvg

--- LetterAvg.java
+++ LetterAvg.java
@@ -13,22 +13,22 @@ public class LetterAvg extends PosAvg	//extends MesoInherit
 	int length;
 	
 	
-	public LetterAvg(char c){
+	public LetterAvg(){
 		super();
 		this.c=c;
 		stations=super.getStations();
 		stID=super.getStID();
 		this.createCharArray();
 		this.createAsciiArray();
-		asciiAV=new int[length];
+		asciiAV=new int[2];
 	}
-
+	/*
 	public LetterAvg() {
 		// TODO Auto-generated constructor stub
 		super();
 		stations=super.getStations();
 	}
-
+	*/
 	public String numberOfStationWithLetterAvg() {
 		// TODO Auto-generated method stub
 		return null;
@@ -57,16 +57,59 @@ public class LetterAvg extends PosAvg	//extends MesoInherit
 	}
 	
 	public void getletterAvg() {
-		
-		int charAverage=0;
+		//vals to find
+		double charAverage=0;
 		int asciiCeil=0;
 		int asciiFloor=0;
 		int asciiAv=0;
 		char asciiAvChar='a';
-		
+		//loop for adding ascii
 		for(int i=0;i<length;i++) {
 			charAverage+=asciiVals[i];
 		}
+		//average computation
+		charAverage/=4;
+		//Math function for vals
+		asciiCeil=(int)Math.ceil(charAverage);
+		asciiCeil=(int)Math.floor(charAverage);
+		//determining average
+		if(charAverage%10>0.5) {
+			asciiAv=asciiCeil;
+		}
+		else {
+			asciiAv=asciiFloor;
+		}
+		//finding average char
+		//asciiAvChar=Character.toString((char)asciiAv);
+		asciiAvChar=(char)asciiAv;
+		//giving asciiAv data
+		asciiAV[0]=asciiCeil;
+		asciiAV[1]=asciiFloor;
+		asciiAV[3]=asciiAv;
+	}
+	public ArrayList<MesoStation> getStations() {
+		return stations;
+	}
+	public char getC() {
+		return c;
+	}
+	public String getStID() {
+		return stID;
+	}
+	public char[] getCharArray() {
+		return charArray;
+	}
+	public int getLetterAvg() {
+		return letterAvg;
+	}
+	public int[] getAsciiVals() {
+		return asciiVals;
+	}
+	public int[] getAsciiAV() {
+		return asciiAV;
+	}
+	public int getLength() {
+		return length;
 	}
 	
 	
