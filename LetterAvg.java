5ffbff0a157fe4c85e42d9551b06635af9f53767
Alexander Caesar <alexjcaesar@yahoo.com>
2019-10-08 15:33:44 -0500

    made multiple arrays for storing data in LetterAvg

--- LetterAvg.java
+++ LetterAvg.java
@@ -2,16 +2,25 @@ import java.util.ArrayList;
 
 public class LetterAvg extends PosAvg	//extends MesoInherit 
 {
+	
 	private ArrayList<MesoStation> stations;
 	char c;
 	String stID;
-	char[] cArray;
+	char[] charArray;
+	int letterAvg;
+	int[] asciiVals;
+	int[] asciiAV;
+	int length;
+	
+	
 	public LetterAvg(char c){
 		super();
 		this.c=c;
 		stations=super.getStations();
 		stID=super.getStID();
 		this.createCharArray();
+		this.createAsciiArray();
+		asciiAV=new int[length];
 	}
 
 	public LetterAvg() {
@@ -25,21 +34,40 @@ public class LetterAvg extends PosAvg	//extends MesoInherit
 		return null;
 	}
 	
-	public int findLength() {
-		
-		return 0;
-	}
-	
 	public void createCharArray() {
 		
-		int length = stID.length();
+		this.length = stID.length();
 		
-		this.cArray=new char[length];
+		this.charArray=new char[length];
 		
 		for(int i=0;i<length;i++) {
-		cArray[i]=stID.charAt(i);
-		}
-		
+		charArray[i]=stID.charAt(i);
+		}	
 	}
+	
+	public void createAsciiArray() {
+		
+		this.length = stID.length();
+		
+		this.asciiVals=new int[length];
+		
+		for(int i=0;i<length;i++) {
+		asciiVals[i]=(int)charArray[i];
+		}	
+	}
+	
+	public void getletterAvg() {
+		
+		int charAverage=0;
+		int asciiCeil=0;
+		int asciiFloor=0;
+		int asciiAv=0;
+		char asciiAvChar='a';
+		
+		for(int i=0;i<length;i++) {
+			charAverage+=asciiVals[i];
+		}
+	}
+	
 	
 }
